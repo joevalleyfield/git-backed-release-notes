779300cc6e1f17ba180be2c9bf3f460836095100
Tim Myers <timothy.j.myers66.civ@army.mil>
2025-09-12 10:06:42 -0400

    feat: add debug logging/perf counters for slow git calls

---
+++ handlers/debug.py
@@ -0,0 +1,20 @@
+from tornado.web import RequestHandler
+from utils.git import get_git_stats, reset_git_stats
+import json
+
+class GitStatsHandler(RequestHandler):
+    def get(self):
+        sort_by = self.get_argument("sort", "total_ms")
+        data = [
+            {"cmd": "git " + " ".join(args),
+             "count": int(s["count"]),
+             "total_ms": round(s["total_ms"], 1),
+             "max_ms": round(s["max_ms"], 1)}
+            for args, s in get_git_stats(sort_by)
+        ]
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(data, indent=2))
+
+    def delete(self):
+        reset_git_stats()
+        self.set_status(204)
